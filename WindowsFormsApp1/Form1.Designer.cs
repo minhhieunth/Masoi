5c64a3030d6aec6612d6cfa6fe87b896076cfb52
minhhieunth <40594830+minhhieunth@users.noreply.github.com>
2019-03-31 15:05:46 +0700

    Login
    Login Form

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDangKyTaiKhoan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDangKyMatKhau = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panelDangNhap = new System.Windows.Forms.Panel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.tbMatKhauDangNhap = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbTaiKhoanDangNhap = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelDangNhap.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(17, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TÀI KHOẢN";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // tbDangKyTaiKhoan
+            // 
+            this.tbDangKyTaiKhoan.Location = new System.Drawing.Point(22, 193);
+            this.tbDangKyTaiKhoan.Name = "tbDangKyTaiKhoan";
+            this.tbDangKyTaiKhoan.Size = new System.Drawing.Size(228, 20);
+            this.tbDangKyTaiKhoan.TabIndex = 2;
+            this.tbDangKyTaiKhoan.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(17, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "MẬT KHẨU";
+            // 
+            // tbDangKyMatKhau
+            // 
+            this.tbDangKyMatKhau.Location = new System.Drawing.Point(22, 260);
+            this.tbDangKyMatKhau.Name = "tbDangKyMatKhau";
+            this.tbDangKyMatKhau.Size = new System.Drawing.Size(228, 20);
+            this.tbDangKyMatKhau.TabIndex = 4;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(97, 311);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(135, 34);
+            this.btnDangKy.TabIndex = 5;
+            this.btnDangKy.Text = "ĐĂNG KÝ";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(63, 366);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(204, 21);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Quay về trang Đăng Nhập";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panelDangNhap
+            // 
+            this.panelDangNhap.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._2;
+            this.panelDangNhap.Controls.Add(this.linkLabel2);
+            this.panelDangNhap.Controls.Add(this.btnDangNhap);
+            this.panelDangNhap.Controls.Add(this.tbMatKhauDangNhap);
+            this.panelDangNhap.Controls.Add(this.label8);
+            this.panelDangNhap.Controls.Add(this.tbTaiKhoanDangNhap);
+            this.panelDangNhap.Controls.Add(this.label7);
+            this.panelDangNhap.Controls.Add(this.label6);
+            this.panelDangNhap.Controls.Add(this.label5);
+            this.panelDangNhap.Controls.Add(this.label4);
+            this.panelDangNhap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDangNhap.Location = new System.Drawing.Point(0, 0);
+            this.panelDangNhap.Name = "panelDangNhap";
+            this.panelDangNhap.Size = new System.Drawing.Size(346, 472);
+            this.panelDangNhap.TabIndex = 7;
+            this.panelDangNhap.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(81, 368);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(172, 19);
+            this.linkLabel2.TabIndex = 8;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(97, 322);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(146, 33);
+            this.btnDangNhap.TabIndex = 7;
+            this.btnDangNhap.Text = "ĐĂNG NHẬP";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // tbMatKhauDangNhap
+            // 
+            this.tbMatKhauDangNhap.Location = new System.Drawing.Point(37, 282);
+            this.tbMatKhauDangNhap.Name = "tbMatKhauDangNhap";
+            this.tbMatKhauDangNhap.Size = new System.Drawing.Size(230, 20);
+            this.tbMatKhauDangNhap.TabIndex = 6;
+            this.tbMatKhauDangNhap.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(33, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 22);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "MẬT KHẨU";
+            // 
+            // tbTaiKhoanDangNhap
+            // 
+            this.tbTaiKhoanDangNhap.Location = new System.Drawing.Point(37, 219);
+            this.tbTaiKhoanDangNhap.Name = "tbTaiKhoanDangNhap";
+            this.tbTaiKhoanDangNhap.Size = new System.Drawing.Size(230, 20);
+            this.tbTaiKhoanDangNhap.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(33, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 22);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "TÀI KHOẢN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(96, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "ĐĂNG NHẬP";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(118, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 14);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Welcome, please login!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(79, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "WEREWOLF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(346, 472);
+            this.Controls.Add(this.panelDangNhap);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.tbDangKyMatKhau);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbDangKyTaiKhoan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelDangNhap.ResumeLayout(false);
+            this.panelDangNhap.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDangKyTaiKhoan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDangKyMatKhau;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panelDangNhap;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbTaiKhoanDangNhap;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbMatKhauDangNhap;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        System.Media.SoundPlayer lobby = new System.Media.SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lobby.SoundLocation = "lobby.wav";
+            lobby.Play();
+        }
+
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panelDangNhap.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            tbTaiKhoanDangNhap.Visible = false;
+            tbMatKhauDangNhap.Visible = false;
+            linkLabel2.Visible = false;
+            btnDangNhap.Visible = false;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panelDangNhap.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            tbTaiKhoanDangNhap.Visible = true;
+            tbMatKhauDangNhap.Visible = true;
+            linkLabel2.Visible = true;
+            btnDangNhap.Visible = true;
+        }
+    }
+}
